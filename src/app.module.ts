6a88e04b0e1194f8ec2abb6b5be84d20e74231c4
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-01-24 23:49:58 +0530

    - created the comments resource with basic crud operations.
    - created the comments resource with basic crud operations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BlogsModule } from './blogs/blogs.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule, BlogsModule],
+  imports: [UsersModule, BlogsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
